ecc8dd67a1f8f5cb4ec496b2e0f049bf5efa6179
matiasrevetria <revetriamatias03@gmail.com>
2024-10-31 09:52:22 -0300

    1.4

--- JS/main.js
+++ JS/main.js
@@ -6,7 +6,5 @@ form.addEventListener("submit",(e)=>{
     if(us.value == ""){
         e.preventDefault();
         err.innerHTML = "El usuario esta vacio..."
-    }else{
-        form.innerHTML = `<h1>Bienvenido, ${us.value}!</h1>` 
     }
 })
