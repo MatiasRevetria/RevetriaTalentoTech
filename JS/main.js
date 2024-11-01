ccc39de3acc154ff8fe83d60588ab332aa24f138
mati <revetriamatias03@gmail.com>
2024-11-01 08:49:09 -0300

    1.5

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,30 @@
 const form = document.getElementById("form");
 const us = document.getElementById("user");
-const err = document.querySelector("#error")
+const us_err = document.querySelector("#error");
+const pass = document.getElementById("password");
+const pass_err = document.querySelector("#error2");
+const cpass = document.getElementById("confirm-password");
+const cpass_err = document.querySelector("#error3");
 
 form.addEventListener("submit",(e)=>{   
     if(us.value == ""){
         e.preventDefault();
-        err.innerHTML = "El usuario esta vacio..."
+        us_err.innerHTML = "El usuario esta vacio..."
+    }else{
+        us_err.innerHTML = ""
+    }
+
+    if(pass.value.length < 8){
+        e.preventDefault();
+        pass_err.innerHTML = "La password debe tener mas de 8 caracteres..."
+    }else{
+        pass_err.innerHTML = ""
+    }
+
+    if(cpass.value != pass.value){
+        e.preventDefault();
+        cpass_err.innerHTML = "Las password no coinciden..."
+    }else{
+        cpasspass_err.innerHTML = ""
     }
 })
